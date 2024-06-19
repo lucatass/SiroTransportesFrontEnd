641f9662b8b7a2fb6ab7346b6794cd7c5eaf5f19
lucatass <lucatas14@gmail.com>
2024-06-19 20:00:07 -0300

    Initial commit with all project files

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { RemitosForm } from "./components/RemitosForm";
+import './App.css';
+
+function App(){
+  return (
+    <div>
+      <RemitosForm />
+    </div>
+  )
+}
+
+export default App
