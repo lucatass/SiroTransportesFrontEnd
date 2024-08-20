213ce280ebce6b020a700b9b992c993810a82264
lucatass <lucatas14@gmail.com>
2024-08-20 13:10:58 -0300

    sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,12 @@ import SidebarButton from './components/SidebarButton';
 import HrutaForm from './components/HRutaForm';
 import RemitoForm from './components/RemitoForm';
 import HRepartoForm from './components/HojaDeReparto/HRepartoForm';
-import { FaTruck, FaFileAlt, FaMapMarkedAlt, FaEdit } from 'react-icons/fa';
+import { FaTruck, FaFileAlt, FaMapMarkedAlt, FaEdit, FaBars } from 'react-icons/fa';
 
 const App: React.FC = () => {
   const [selectedComponent, setSelectedComponent] = useState<string>('HRepartoForm');
+  const [isDropdownOpen, setIsDropdownOpen] = useState<boolean>(false);
+  const [isNestedDropdownOpen, setIsNestedDropdownOpen] = useState<boolean>(false);
 
   const renderComponent = () => {
     switch (selectedComponent) {
@@ -25,28 +27,47 @@ const App: React.FC = () => {
 
   return (
     <div className="app">
-      <div className="sidebar">
-        <SidebarSection label="Facturación" icon={<FaFileAlt  />}>
-          <SidebarButton
-            icon={<FaTruck />}
-            label="HReparto"
-            onClick={() => setSelectedComponent('HRepartoForm')}
-          />
-          <SidebarButton
-            icon={<FaMapMarkedAlt />}
-            label="HRuta"
-            onClick={() => setSelectedComponent('HrutaForm')}
-          />
-          <SidebarButton
-            icon={<FaEdit />}
-            label="Remito"
-            onClick={() => setSelectedComponent('RemitoForm')}
-          />
+      <div
+        className="dropdown-toggle"
+        onClick={() => setIsDropdownOpen(!isDropdownOpen)}
+      >
+        <FaBars />
+      </div>
+      <div className={`sidebar-dropdown ${isDropdownOpen ? 'open' : ''}`}>
+        <SidebarSection
+          label="Facturación"
+          icon={<FaFileAlt />}
+          onClick={() => setIsNestedDropdownOpen(!isNestedDropdownOpen)}
+        >
+          <div className={`nested-dropdown ${isNestedDropdownOpen ? 'open' : ''}`}>
+            <SidebarButton
+              icon={<FaTruck />}
+              label="HReparto"
+              onClick={() => {
+                setSelectedComponent('HRepartoForm');
+                setIsNestedDropdownOpen(false);
+              }}
+            />
+            <SidebarButton
+              icon={<FaMapMarkedAlt />}
+              label="HRuta"
+              onClick={() => {
+                setSelectedComponent('HrutaForm');
+                setIsNestedDropdownOpen(false);
+              }}
+            />
+            <SidebarButton
+              icon={<FaEdit />}
+              label="Remito"
+              onClick={() => {
+                setSelectedComponent('RemitoForm');
+                setIsNestedDropdownOpen(false);
+              }}
+            />
+          </div>
         </SidebarSection>
       </div>
-      <div className="content">
-        {renderComponent()}
-      </div>
+      <div className="content">{renderComponent()}</div>
     </div>
   );
 };
